0f687fbe8491de0e1f5bb6f215ce13a9437b1172
pAIgn10 <pAIgn10.ln@gmail.com>
2015-05-01 02:15:33 +0300

    Added docs for v1.1

--- 1.0/search/classes_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['rgbnorm',['RGBNorm',['../classcl__algo_1_1RGBNorm.html',1,'cl_algo']]]
-];
---
+++ 1.1/search/classes_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['rgbdto8d',['RGBDTo8D',['../classcl__algo_1_1RGBDTo8D.html',1,'cl_algo']]],
+  ['rgbnorm',['RGBNorm',['../classcl__algo_1_1RGBNorm.html',1,'cl_algo']]]
+];
